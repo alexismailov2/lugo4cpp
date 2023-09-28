333295b61fadaf507ea7a13e830ce0299a6b5a6f
Alexander Ismailov <tama.rilla@protonmail.com>
2023-09-28 21:09:32 +0200

    Added build script for grpc and its dependencies.
    Signed-off-by: Alexander Ismailov <tama.rilla@protonmail.com>

---
+++ build_grpc.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+sudo apt-get install -y build-essential autoconf libtool pkg-config cmake clang libc++-dev
+
+git clone -b v1.58.0 https://github.com/grpc/grpc
+cd grpc
+git submodule update --init --recursive
+
+# Install absl
+mkdir -p "third_party/abseil-cpp/cmake/build"
+pushd "third_party/abseil-cpp/cmake/build"
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE ../..
+make install
+popd
+
+# Install c-ares
+# If the distribution provides a new-enough version of c-ares,
+# this section can be replaced with:
+# apt-get install -y libc-ares-dev
+mkdir -p "third_party/cares/cares/cmake/build"
+pushd "third_party/cares/cares/cmake/build"
+cmake -DCMAKE_BUILD_TYPE=Release ../..
+make install
+popd
+
+# Install protobuf
+mkdir -p "third_party/protobuf/cmake/build"
+pushd "third_party/protobuf/cmake/build"
+cmake -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release -Dprotobuf_ABSL_PROVIDER=package ../..
+make
+popd
+
+# Install re2
+mkdir -p "third_party/re2/cmake/build"
+pushd "third_party/re2/cmake/build"
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE ../..
+make
+popd
+
+# Install zlib
+mkdir -p "third_party/zlib/cmake/build"
+pushd "third_party/zlib/cmake/build"
+cmake -DCMAKE_BUILD_TYPE=Release ../..
+make
+popd
+
+cmake -Bbuild \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DgRPC_INSTALL=ON \
+      -DgRPC_ABSL_PROVIDER=package \
+      -DgRPC_CARES_PROVIDER=package \
+      -DgRPC_PROTOBUF_PROVIDER=package \
+      -DgRPC_RE2_PROVIDER=package \
+      -DgRPC_SSL_PROVIDER=package \
+      -DgRPC_ZLIB_PROVIDER=package
+      #-DCMAKE_INSTALL_PREFIX=${{github.workspace}}/grpc/grpc_install
+cmake --build build
+
